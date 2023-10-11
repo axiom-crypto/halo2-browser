7ed4ca0f45396eaba3f85bc91250edcbcba4ed20
Yu Jiang Tham <yoojbruin@gmail.com>
2023-10-11 14:02:46 -0700

    Update cargo script

--- scripts/updateCargoVersion.js
+++ scripts/updateCargoVersion.js
@@ -9,18 +9,16 @@ async function updateCargoVersion() {
     input: fileStream,
     crlfDelay: Infinity,
   });
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
 
   let newFile = "";
   for await (const line of rl) {
     console.log(line.replace('\\', '+'));
     if (line.startsWith("version = ")) {
       const newVersion = `version = "${packageJson.version}"`;
-      newFile += newVersion + "\r\n";
+      newFile += newVersion + "\n";
       continue;
     }
-    newFile += line + "\r\n";
+    newFile += line + "\n";
   }
 
   fs.writeFileSync('./Cargo.toml', newFile);
