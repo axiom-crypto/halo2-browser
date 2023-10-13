ecc9fb6e21fa9c564236ec0289807b5a3a9509c4
Roshan <19766713+rpalakkal@users.noreply.github.com>
2023-10-13 17:27:19 -0400

    Feat: add kzg fetching and web/js scaffolds (#4)

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,3 +6,11 @@ mkdir pkg
 ./scripts/build-js.sh
 ./scripts/build-web.sh
 node ./scripts/makePkg.js
+
+npx tsc
+
+sed -i '' 's#\.\./\.\./pkg/js/halo2_wasm#./halo2_wasm#g' ./pkg/js/index.d.ts
+sed -i '' 's#\.\./\.\./pkg/js/halo2_wasm#./halo2_wasm#g' ./pkg/js/index.js
+
+sed -i '' 's#\.\./\.\./pkg/web/halo2_wasm#./halo2_wasm#g' ./pkg/web/index.d.ts
+sed -i '' 's#\.\./\.\./pkg/web/halo2_wasm#./halo2_wasm#g' ./pkg/web/index.js
