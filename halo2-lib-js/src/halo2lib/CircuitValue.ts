664fba38d5984262fcdf6bc6e86f00940387e35d
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2023-10-30 23:14:49 -0700

    chore: fix nits

--- halo2-lib-js/src/halo2lib/CircuitValue.ts
+++ halo2-lib-js/src/halo2lib/CircuitValue.ts
@@ -2,51 +2,58 @@ import { Halo2LibWasm } from "@axiom-crypto/halo2-wasm/web";
 import { CircuitValue256 } from "./CircuitValue256";
 
 export class CircuitValue {
-    private _value: bigint;
-    private _cell: number;
-    private _circuit: Halo2LibWasm;
+  private _value: bigint;
+  private _cell: number;
+  private _circuit: Halo2LibWasm;
 
-    constructor(circuit: Halo2LibWasm, { value, cell }: { value?: bigint | number | string, cell?: number }) {
-        this._circuit = circuit;
-        if (value !== undefined) {
-            this._value = BigInt(value);
-            this._cell = this._circuit.constant(value.toString());
-        }
-        else if (cell !== undefined) {
-            this._cell = cell;
-            const val = BigInt(circuit.value(cell));
-            this._value = val;
-        }
-        else {
-            throw new Error("Invalid input");
-        }
+  constructor(
+    circuit: Halo2LibWasm,
+    { value, cell }: { value?: bigint | number | string; cell?: number }
+  ) {
+    this._circuit = circuit;
+    if (value !== undefined) {
+      this._value = BigInt(value);
+      this._cell = this._circuit.constant(value.toString());
+    } else if (cell !== undefined) {
+      this._cell = cell;
+      const val = BigInt(circuit.value(cell));
+      this._value = val;
+    } else {
+      throw new Error("Invalid input");
     }
+  }
 
-    cell() {
-        return this._cell;
-    }
+  cell() {
+    return this._cell;
+  }
 
-    value() {
-        return this._value;
-    }
+  value() {
+    return this._value;
+  }
 
-    number() {
-        return Number(this._value);
-    }
+  number() {
+    return Number(this._value);
+  }
 
-    address() {
-        return "0x" + this._value.toString(16).padStart(40, '0');
-    }
+  address() {
+    return "0x" + this._value.toString(16).padStart(40, "0");
+  }
 
-    toCircuitValue256() {
-        const b = BigInt(2) ** BigInt(128);
-        const bCell = this._circuit.constant(b.toString());
-        const lookupBits = this._circuit.lookup_bits();
-        let paddedNumBits = Math.floor(253 / lookupBits) * lookupBits - 1;
-        const [hi, lo] = this._circuit.div_mod_var(this._cell, bCell, paddedNumBits.toString(), "129")
-        const hi128CircuitValue = new CircuitValue(this._circuit, { cell: hi });
-        const lo128CircuitValue = new CircuitValue(this._circuit, { cell: lo });
-        const halo2LibValue256 = new CircuitValue256(this._circuit, { hi: hi128CircuitValue, lo: lo128CircuitValue });
-        return halo2LibValue256;
-    }
+  toCircuitValue256() {
+    const b = 2n ** 128n;
+    const lookupBits = this._circuit.lookup_bits();
+    let paddedNumBits = Math.floor(253 / lookupBits) * lookupBits - 1;
+    const [hi, lo] = this._circuit.div_mod(
+      this._cell,
+      b.toString(),
+      paddedNumBits.toString()
+    );
+    const hi128CircuitValue = new CircuitValue(this._circuit, { cell: hi });
+    const lo128CircuitValue = new CircuitValue(this._circuit, { cell: lo });
+    const halo2LibValue256 = new CircuitValue256(this._circuit, {
+      hi: hi128CircuitValue,
+      lo: lo128CircuitValue,
+    });
+    return halo2LibValue256;
+  }
 }
--- halo2-lib-js/src/halo2lib/CircuitValue256.ts
+++ halo2-lib-js/src/halo2lib/CircuitValue256.ts
@@ -3,59 +3,91 @@ import { CircuitValue } from "./CircuitValue";
 import { convertInput } from "../shared/utils";
 
 export class CircuitValue256 {
-    private _value: bigint;
-    private _circuitValue: [CircuitValue, CircuitValue];
-    private _halo2Lib: Halo2LibWasm;
+  private _value: bigint;
+  private _circuitValue: [CircuitValue, CircuitValue];
+  private _halo2Lib: Halo2LibWasm;
 
-    constructor(_halo2Lib: Halo2LibWasm, { value, hi, lo }: { value?: bigint | string | number, hi?: CircuitValue, lo?: CircuitValue }) {
-        this._halo2Lib = _halo2Lib;
-        if (value !== undefined) {
-            this._value = BigInt(value);
-            let input = BigInt(value).toString(16).padStart(64, '0');
-            let hi128 = input.slice(0, 32);
-            let lo128 = input.slice(32);
-
-            const hi128CircuitValue = new CircuitValue(_halo2Lib, { cell: _halo2Lib.constant(convertInput("0x" + hi128)) });
-            const lo128CircuitValue = new CircuitValue(_halo2Lib, { cell: _halo2Lib.constant(convertInput("0x" + lo128)) });
-            this._circuitValue = [hi128CircuitValue, lo128CircuitValue];
-        }
-        else if (hi !== undefined && hi instanceof CircuitValue && lo !== undefined && lo instanceof CircuitValue) {
-            this._circuitValue = [hi, lo];
-            const hiVal = BigInt(_halo2Lib.value(hi.cell()));
-            const loVal = BigInt(_halo2Lib.value(lo.cell()));
-            const value = hiVal * BigInt(2 ** 128) + loVal;
-            this._value = value;
-        }
-        else {
-            throw new Error("Invalid input");
-        }
+  constructor(
+    _halo2Lib: Halo2LibWasm,
+    {
+      value,
+      hi,
+      lo,
+    }: {
+      value?: bigint | string | number;
+      hi?: CircuitValue;
+      lo?: CircuitValue;
     }
+  ) {
+    this._halo2Lib = _halo2Lib;
+    if (value !== undefined) {
+      if (BigInt(value) < 0n) {
+        throw new Error("Value cannot be negative.");
+      }
+      this._value = BigInt(value);
+      let input = BigInt(value)
+        .toString(16)
+        .padStart(64, "0");
+      let hi128 = input.slice(0, 32);
+      let lo128 = input.slice(32);
 
-    hi() {
-        return this._circuitValue[0];
+      const hi128CircuitValue = new CircuitValue(_halo2Lib, {
+        cell: _halo2Lib.constant(convertInput("0x" + hi128)),
+      });
+      const lo128CircuitValue = new CircuitValue(_halo2Lib, {
+        cell: _halo2Lib.constant(convertInput("0x" + lo128)),
+      });
+      this._circuitValue = [hi128CircuitValue, lo128CircuitValue];
+    } else if (
+      hi !== undefined &&
+      hi instanceof CircuitValue &&
+      lo !== undefined &&
+      lo instanceof CircuitValue
+    ) {
+      this._circuitValue = [hi, lo];
+      const hiVal = BigInt(_halo2Lib.value(hi.cell()));
+      const loVal = BigInt(_halo2Lib.value(lo.cell()));
+      const value = hiVal * 2n ** 128n + loVal;
+      this._value = value;
+    } else {
+      throw new Error("Invalid input");
     }
+  }
 
-    lo() {
-        return this._circuitValue[1];
+  hi() {
+    return this._circuitValue[0];
+  }
+
+  lo() {
+    return this._circuitValue[1];
+  }
+
+  hex() {
+    return "0x" + this._value.toString(16).padStart(64, "0");
+  }
+
+  value() {
+    return this._value;
+  }
+
+  toCircuitValue() {
+    const b = BigInt(2) ** BigInt(128);
+    const bCell = this._halo2Lib.constant(b.toString());
+    const cell = this._halo2Lib.mul_add(
+      this.hi().cell(),
+      bCell,
+      this.lo().cell()
+    );
+    this._halo2Lib.range_check(this.hi().cell(), "125");
+    if (
+      this.hi()
+        .value()
+        .toString(2).length > 125
+    ) {
+      throw new Error(
+        "Cannot convert to CircuitValue (value is > 253 bits). Please use .hi()/.lo() instead."
+      );
     }
-
-    hex() {
-        return "0x" + this._value.toString(16).padStart(64, '0');
-    }
-
-    value() {
-        return this._value;
-    }
-
-    toCircuitValue() {
-        const b = BigInt(2) ** BigInt(128);
-        const bCell = this._halo2Lib.constant(b.toString());
-        const cell = this._halo2Lib.mul_add(this.hi().cell(), bCell, this.lo().cell());
-        this._halo2Lib.range_check(this.hi().cell(), "125");
-        if (this.hi().value().toString(2).length > 125) {
-            throw new Error("Cannot convert to CircuitValue (value is > 253 bits). Please use .hi()/.lo() instead.");
-        }
-        return new CircuitValue(this._halo2Lib, { cell })
-    }
-
+    return new CircuitValue(this._halo2Lib, { cell });
+  }
 }
