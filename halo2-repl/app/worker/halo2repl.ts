a0fd5ef6d1b31c1affcd2ec2fd6180f1fbb412a4
Roshan <19766713+rpalakkal@users.noreply.github.com>
2023-11-01 09:48:48 -0400

    fix: default starting config

--- halo2-repl/app/worker/halo2repl.ts
+++ halo2-repl/app/worker/halo2repl.ts
@@ -4,6 +4,11 @@ import { Halo2CircuitRunner, captureConsoleOutput } from '@axiom-crypto/halo2-li
 import { Halo2Lib } from '@axiom-crypto/halo2-lib-js';
 import { Halo2LibWasm, getHalo2LibWasm, CircuitScaffold, getKzgParams, DEFAULT_CIRCUIT_CONFIG } from '@axiom-crypto/halo2-wasm/web'
 
+const DEFAULT_HALO2REPL_CONFIG = {
+    ... DEFAULT_CIRCUIT_CONFIG,
+    k: 20,
+    numLookupBits: 19,
+}
 
 export class Halo2Repl extends CircuitScaffold {
 
@@ -27,7 +32,7 @@ export class Halo2Repl extends CircuitScaffold {
     async populateCircuit(rawCode: string, appInputs: string) {
         this.code = rawCode + "\n";
         this.inputs = appInputs;
-        this.newCircuitFromConfig(DEFAULT_CIRCUIT_CONFIG);
+        this.newCircuitFromConfig(DEFAULT_HALO2REPL_CONFIG);
         console.time("Witness generation")
         const { config } = await Halo2CircuitRunner(this.halo2wasm, this.halo2Lib, this.config).runFromString(this.code, this.inputs)
         this.config = config;
