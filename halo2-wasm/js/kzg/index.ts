025948b1f1d8aacd756a059fe902e152fc361bc7
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-01-15 10:09:35 -0500

    feat: cache Axiom SRS (trusted setup)

--- halo2-wasm/js/kzg/index.ts
+++ halo2-wasm/js/kzg/index.ts
@@ -1,28 +1,36 @@
+import * as os from "os";
+import * as path from "path";
+import * as fs from "fs";
+
 function fetchAndConvertToUint8Array(url: string): Promise<Uint8Array> {
   return new Promise((resolve, reject) => {
     // Check if running in Node.js environment
-    if (typeof process !== 'undefined' && process.versions && process.versions.node) {
-      const https = require('https');
+    if (
+      typeof process !== "undefined" &&
+      process.versions &&
+      process.versions.node
+    ) {
+      const https = require("https");
       https.get(url, (res: any) => {
         let chunks: any[] = [];
-        res.on('data', (chunk: any) => chunks.push(chunk));
-        res.on('end', () => {
+        res.on("data", (chunk: any) => chunks.push(chunk));
+        res.on("end", () => {
           let binaryData = Buffer.concat(chunks);
           resolve(new Uint8Array(binaryData));
         });
-        res.on('error', reject);
+        res.on("error", reject);
       });
     }
     // Check if running in browser or web worker environment
-    else if (typeof window !== 'undefined' || typeof self !== 'undefined') {
+    else if (typeof window !== "undefined" || typeof self !== "undefined") {
       fetch(url)
-        .then(response => response.arrayBuffer())
-        .then(buffer => {
+        .then((response) => response.arrayBuffer())
+        .then((buffer) => {
           resolve(new Uint8Array(buffer));
         })
         .catch(reject);
     } else {
-      reject(new Error('Environment not supported'));
+      reject(new Error("Environment not supported"));
     }
   });
 }
@@ -34,11 +42,33 @@ const convertBase64ToUint8Arr = (b64str: string) => {
     buf[i] = ch.charCodeAt(0);
   });
   return buf;
-}
+};
 
 export const getKzgParams = async (k: number): Promise<Uint8Array> => {
+  const home = os.homedir();
+  const axiomSrsPath = path.join(
+    home,
+    ".axiom",
+    "srs",
+    "challenge_0085",
+    `kzg_bn254_${k}.srs`
+  );
+  const exists = fs.existsSync(axiomSrsPath);
+  if (exists) {
+    const buffer = fs.readFileSync(axiomSrsPath);
+    return new Uint8Array(buffer);
+  }
+  const folderPath = path.dirname(axiomSrsPath);
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
   if (k < 6 || k > 19) {
     throw new Error(`k=${k} is not supported`);
   }
-  return fetchAndConvertToUint8Array(`https://axiom-crypto.s3.amazonaws.com/challenge_0085/kzg_bn254_${k}.srs`);
+  const srs = await fetchAndConvertToUint8Array(
+    `https://axiom-crypto.s3.amazonaws.com/challenge_0085/kzg_bn254_${k}.srs`
+  );
+  fs.writeFileSync(axiomSrsPath, srs);
+
+  return srs;
 };
