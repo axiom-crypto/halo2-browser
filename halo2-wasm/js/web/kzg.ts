262b66df4aeed72c6aea95b8732de6c4e007e61e
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-15 16:59:42 -0800

    feat/cache-srs tmp changes

--- halo2-wasm/js/web/kzg.ts
+++ halo2-wasm/js/web/kzg.ts
@@ -1,8 +1,9 @@
 import { fetchAndConvertToUint8Array } from "../shared/utils";
 
 export const getKzgParams = async (k: number): Promise<Uint8Array> => {
+  console.log("KZG: web");
   if (k < 6 || k > 19) {
     throw new Error(`k=${k} is not supported`);
   }
-  return fetchAndConvertToUint8Array(`https://axiom-crypto.s3.amazonaws.com/challenge_0085/kzg_bn254_${k}.srs`);
+  return await fetchAndConvertToUint8Array(`https://axiom-crypto.s3.amazonaws.com/challenge_0085/kzg_bn254_${k}.srs`);
 };
