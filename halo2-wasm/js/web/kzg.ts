c951677c5fa6f57818f8e3938963d3d623cfe88c
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-15 17:28:56 -0800

    WIP

--- halo2-wasm/js/web/kzg.ts
+++ halo2-wasm/js/web/kzg.ts
@@ -5,5 +5,7 @@ export const getKzgParams = async (k: number): Promise<Uint8Array> => {
   if (k < 6 || k > 19) {
     throw new Error(`k=${k} is not supported`);
   }
-  return await fetchAndConvertToUint8Array(`https://axiom-crypto.s3.amazonaws.com/challenge_0085/kzg_bn254_${k}.srs`);
+  const srs = await fetchAndConvertToUint8Array(`https://axiom-crypto.s3.amazonaws.com/challenge_0085/kzg_bn254_${k}.srs`)
+  console.log("KZG: web", srs);
+  return srs;
 };
