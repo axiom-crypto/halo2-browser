8299fcf81ae287db4fdffd7424b0cd107d18a178
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-17 09:17:53 -0800

    Update toggle local scripts for ci

--- halo2-lib-js/scripts/toggleLocal.js
+++ halo2-lib-js/scripts/toggleLocal.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const packageJson = require('../package.json');
 
+let ci = false;
+if (process.argv[2] === "ci") {
+    ci = true;
+}
+const local = ci ? "file:" : "link:";
+
 const main = () => {
     const packageJsonCopy = { ...packageJson };
-    packageJsonCopy.dependencies['@axiom-crypto/halo2-wasm'] = 'link:../halo2-wasm/pkg';
+    packageJsonCopy.dependencies['@axiom-crypto/halo2-wasm'] = `${local}../halo2-wasm/pkg`;
     fs.writeFileSync('./package.json', JSON.stringify(packageJsonCopy, null, 2));
 }
 
--- scripts/toggleLocal.sh
+++ scripts/toggleLocal.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+
+CI=""
+if [ "$1" = "ci" ]; then
+    CI="ci"
+fi
+echo $CI
+
 ./scripts/build.sh
 cd halo2-lib-js
-node ./scripts/toggleLocal.js
+node ./scripts/toggleLocal.js $CI
 pnpm install
 cd ../halo2-repl
-node ./scripts/toggleLocal.js
+node ./scripts/toggleLocal.js $CI
 pnpm install
 cd ../cli
-node ./scripts/toggleLocal.js
+node ./scripts/toggleLocal.js $CI
 pnpm install
